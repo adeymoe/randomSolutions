6dadbda1f702cee8c44a1376f3ff3ae22bf2c49e
adeymoe <araoyed@gmail.com>
2024-06-07 09:06:08 +0100

    adding popping first and last element

--- sliceArray.py
+++ sliceArray.py
@@ -1,2 +1,28 @@
 def take(arr,n):
     return arr[:n]
+
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+def mod(a, b):
+    return a % b
+
+def exponent(a, b):
+    return a ** b
+
+def subt(a, b):
+    return a - b
+
+
+
+def remove_char(s):
+    s = list(s)
+    s.pop()
+    s.pop(0)
+    return ''.join(s)
